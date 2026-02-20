390893868c1bffb7932688fd89e10493970611c4
Ashwath <ashwath.singh2005@gmail.com>
2026-02-19 22:48:00 -0500

    fixed breaking of lsp and formatting

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,13 +2,11 @@ return {
   {
     "neovim/nvim-lspconfig",
     keys = {
-      -- Move code actions to <leader>ca
       {
         "<leader>ca",
         vim.lsp.buf.code_action,
         desc = "Code Action",
       },
-      -- Disable old mapping
       {
         "<leader>c",
         false,
